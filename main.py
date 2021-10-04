c675d930f049374cd7552bec1f74c33ba60cef18
b0mbarnac <36277010+b0mbarnac@users.noreply.github.com>
2021-10-04 14:16:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+colors = {
+    0: 'black',
+    1: 'r',
+    2: 'y',
+    3: 'b',
+    4: 'g',
+    5: 'lime',
+    6: 'c',
+    7: 'coral',
+    8: 'pink',
+    9: 'purple'
+}
+
 
 def dist(x1, y1, x2, y2):
     return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
@@ -77,11 +90,11 @@ if __name__ == '__main__':
     result = [0]
     points = rand_points()
 
-    while k != 10:
+    while k != 12:
         m = 0
         count = 0
         result.append(0)
-        tmp_centroid = []
+
         coord_c = center(points[0], points[1], k)
         while m != 1:
             count += 1
@@ -96,29 +109,70 @@ if __name__ == '__main__':
             for i in range(len(what_centr_tmp)):
                 what_centr_tmp[i] = newCoordCentr(points, tmp_centroid, i)
             new_coord_c = to_normal_array(what_centr_tmp)
-
-            # plt.scatter(points[0], points[1], color='b')
+            # plt.ion()
+            # plt.scatter(points[0], points[1], color=colors[1])
+            # plt.draw()
+            # plt.pause(1)
             # plt.scatter(coord_c[0], coord_c[1], color='r')
+            # plt.draw()
+            # plt.pause(1)
             # plt.scatter(new_coord_c[0], new_coord_c[1], color='y')
+            # plt.draw()
+            # plt.pause(1)
             # plt.show()
+            # plt.ioff()
 
             for i in range(k):
                 if new_coord_c[0][i] != coord_c[0][i] or new_coord_c[1][i] != coord_c[1][i]:
                     m = 0
                 coord_c[0][i] = new_coord_c[0][i]
                 coord_c[1][i] = new_coord_c[1][i]
-        # print(count)
-        # result +=
-        # print('coord_c = ' + str(coord_c))
-        # print('tmp = ' + str(tmp_centroid))
+            # plt.clf()
         for i in range(len(points[0])):
             result[k - 1] += square_dist(
                 dist(coord_c[0][tmp_centroid[i]], coord_c[1][tmp_centroid[i]], points[0][i], points[1][i]))
-        # print('summa with ' + str(k) + ' klasterami = ' + str(result[k - 1]))
         k += 1
 
     D_k = []
     for i in range(1, k - 1):
         D_k.append(result[i] - result[i + 1])
-    plt.scatter([1, 2, 3, 4, 5, 6, 7, 8], D_k, color='r')
-    plt.show()
+
+    min_D = 0
+    for i in range(len(D_k)):
+        if D_k[i] < D_k[min_D]:
+            min_D = i
+    min_D += 1
+    m = 0
+    coord_c = center(points[0], points[1], min_D)
+    while m != 1:
+        m = 1
+        what_centr_tmp = []
+        for i in range(min_D):
+            what_centr_tmp.append(0)
+        tmp_centroid = []
+        for i in range(len(points[0])):
+            tmp_centroid.append(
+                num_of_centroid(coord_c, points[0][i], points[1][i]))
+        for i in range(len(what_centr_tmp)):
+            what_centr_tmp[i] = newCoordCentr(points, tmp_centroid, i)
+        new_coord_c = to_normal_array(what_centr_tmp)
+        plt.ion()
+        plt.plot(100, 100)
+        # plt.draw()
+        for i in range(len(tmp_centroid)):
+            plt.scatter(points[0][i], points[1][i], color=colors[tmp_centroid[i]])
+            plt.draw()
+
+        for i in range(len(coord_c[0])):
+            plt.scatter(coord_c[0][i], coord_c[1][i], color=colors[i],  s=100, edgecolors='black', marker='*')
+            plt.draw()
+        plt.pause(2)
+        plt.show()
+        plt.ioff()
+        plt.clf()
+        for i in range(min_D):
+            if new_coord_c[0][i] != coord_c[0][i] or new_coord_c[1][i] != coord_c[1][i]:
+                m = 0
+            coord_c[0][i] = new_coord_c[0][i]
+            coord_c[1][i] = new_coord_c[1][i]
+
